ee64490a188e89fcefc8d2bb4e974d84ed1acd42
Rod <48662639+rodwestmoreland@users.noreply.github.com>
2021-03-22 10:14:36 -0400

    Add project files.

---
+++ 1150.RestaurantRater/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1150.RestaurantRater.MvcApplication" Language="C#" %>
---
+++ 1150.RestaurantRater/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1150.RestaurantRater.Startup))]
+namespace _1150.RestaurantRater
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
